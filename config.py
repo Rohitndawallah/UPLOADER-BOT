07384223f6833916d17b6d18c37ab1a9c177e15a
JEEx07 <141727899+Rohitndawallah@users.noreply.github.com>
2025-07-07 16:53:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config(object):
     # get a token from @BotFather
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "8033973885:AAFWPSNJVlNWHRl3hDd0lo41m70M7_n68ok")
     # The Telegram API things
-    API_ID = int(os.environ.get("API_ID", "24616136")
+    API_ID = int(os.environ.get("API_ID", "24616136"))
     API_HASH = os.environ.get("API_HASH", "2554bc329f42eb20b0d34bded2847e22")
     # Get these values from my.telegram.org
     # the download location, where the HTTP Server runs
